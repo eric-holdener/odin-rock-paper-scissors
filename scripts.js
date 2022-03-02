60516fbe0814ecb6ad3cfb9b8e6c8946735a8467
EHoldener <eholdener12@gmail.com>
2022-03-01 17:55:52 -0800

    Added dropdown list for player to pick their move, rock, paper, or scissors, and made a fucntion playerselection to scan that list and return a value of rock, paper, or scissors based on player input

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 function computerPlay() {
     let playSelector = Math.floor(Math.random() * 3) + 1;
-    console.log(playSelector)
     if (playSelector == 1) {
-        let computerChoice = "Rock"
-        console.log(computerChoice)
-        return computerChoice
+        let computerChoice = "Rock";
+        return computerChoice;
     } else if (playSelector == 2) {
-        let computerChoice = "Paper"
-        console.log(computerChoice)
-        return computerChoice
+        let computerChoice = "Paper";
+        return computerChoice;
     } else {
-        let computerChoice = "Scissors"
-        console.log(computerChoice)
-        return computerChoice
+        let computerChoice = "Scissors";
+        return computerChoice;
     }
+}
+
+function playerSelection() {
+    let playerChoice = document.getElementById('player-choice');
+    return playerChoice.value;
 }
