a5028d8ee7b06e46d5ce6e934e2c8500e54d933c
EHoldener <eholdener12@gmail.com>
2022-03-06 16:52:03 -0800

    bugfixing

--- scripts.js
+++ scripts.js
@@ -15,38 +15,38 @@ function computerPlay() {
 function playRound(computerChoice, playerChoice) {
     if (computerChoice == "Rock" && playerChoice == "Rock") {
         let winner = "tie";
-        return [winner, computerChoice];
+        return winner;
     }
     else if (computerChoice == "Rock" && playerChoice == "Paper") {
         let winner = "player";
-        return [winner, computerChoice];
+        return winner;
     }
     else if (computerChoice == "Rock" && playerChoice == "Scissors") {
         let winner = "computer";
-        return [winner, computerChoice];
+        return winner;
     }
     else if (computerChoice == "Paper" && playerChoice == "Paper") {
         let winner = "tie";
-        return [winner, computerChoice];
+        return winner;
     }
     else if (computerChoice == "Paper" && playerChoice == "Rock") {
         let winner = "computer";
-        return [winner, computerChoice];
+        return winner;
     }
     else if (computerChoice == "Paper" && playerChoice == "Scissors") {
         let winner = "player";
-        return [winner, computerChoice];
+        return winner;
     }
     else if (computerChoice == "Scissors" && playerChoice == "Scissors") {
         let winner = "tie";
-        return [winner, computerChoice];
+        return winner;
     }
     else if (computerChoice == "Scissors" && playerChoice == "Paper") {
         let winner = "computer";
         return winner;
     }
     else if (computerChoice == "Scissors" && playerChoice == "Rock") {
-        let winner = "player"
+        let winner = "player";
         return winner;
     } else {
         winner = "no one";
@@ -56,18 +56,18 @@ function playRound(computerChoice, playerChoice) {
 
 function game(playerChoice) {
     let computerChoice = computerPlay();
-
+    
     let winner = playRound(computerChoice, playerChoice);
 
+    determineWinner(winner, playerChoice, computerChoice);
+}
 
+function determineWinner(winner, playerChoice, computerChoice) {
     if (winner == "player") {
         console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
     } else if (winner == "computer") {
         console.log(`You lose! ${computerChoice} beats ${playerChoice}.`);
     } else if (winner == "tie") {
         console.log(`You tie! You both picked ${computerChoice}.`);
-    } else {
-        console.log("Maybe there was a typo? You didn't pick a valid answer. Try again")
-            i -= 1;
     }
 }
